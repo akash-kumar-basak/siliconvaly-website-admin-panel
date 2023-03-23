62ca952d25227a138b92c069d7e224e15eba2b97
Akash kumar basak <akash4116@gmail.com>
2023-03-24 01:56:16 +0600

    add
    add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\CompanyModel;
+use App\Models\backend\CompanyModel;
 
 class DatabaseSeeder extends Seeder
 {
