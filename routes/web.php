28f2be11ea5293722fa572d79d879f5b7ee51873
Akash kumar basak <akash4116@gmail.com>
2023-05-27 11:24:08 -0700

    add
    add

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,5 @@ Route::resource('product', ProductController::class);
 //---------------frontend------------------------
 Route::get('/', [homeController::class, 'home']);
 Route::get('/product/{category}/{id}', [homeController::class, 'productDetails']);
-Route::get('/product_carts', [homeController::class, 'productCart']);
-Route::get('/product_checkouts', [homeController::class, 'productCheckout']);
+Route::get('/product_cart', [homeController::class, 'productCart']);
+Route::get('/product_checkout', [homeController::class, 'productCheckout']);
